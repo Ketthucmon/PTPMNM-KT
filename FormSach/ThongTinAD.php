f39e297c4c35a3ad760ba6e5d3238815bed7e7e0
phucbui1b <phucbui1b@gmail.com>
2017-07-15 23:15:21 +0700

    Page người kiểm kê sách

---
+++ FormSach/ThongTinAD.php
@@ -0,0 +1,14 @@
+
+<table width="800px" id="main-content" border="0px" cellpadding="0px" cellspacing="0px">
+            	<tr id="main-navbar" height="36px"class="menu-item" >
+                	<td colspan="6">giới thiệu</td>
+                </tr>
+				<tr>
+                	<td align="justify" id="info"><b>Chào mừng bạn đến với hệ thống quản lý cửa hàng sách PHƯƠNG NAM của người kiểm kê sách.Trang Web gồm những chức năng như sau:<br/>
+                    -<b>Đối Tác Nhà Xuất Bản</b><br/>
+                    -<b>Thông Tin Kho Sách</b><br/>
+                    -<b>Thông Tin Người Dùng</b>          
+                   
+                    </td>
+                </tr>
+            </table>
