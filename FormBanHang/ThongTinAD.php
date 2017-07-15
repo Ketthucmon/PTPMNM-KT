f572e280c567e37eee6e160e02b506fa1ed7afd0
phucbui1b <phucbui1b@gmail.com>
2017-07-15 23:13:26 +0700

    page của người bán hàng

---
+++ FormBanHang/ThongTinAD.php
@@ -0,0 +1,15 @@
+
+<table width="800px" id="main-content" border="0px" cellpadding="0px" cellspacing="0px">
+            	<tr id="main-navbar" height="36px"class="menu-item" >
+                	<td colspan="6">giới thiệu</td>
+                </tr>
+				<tr>
+                	<td align="justify" id="info"><b>Chào mừng bạn đến với hệ thống quản lý cửa hàng sách PHƯƠNG NAM của bán hàng.Trang Web gồm những chức năng như sau:<br/>
+
+                    -<b>Thông Tin Hóa Đơn</b><br/>
+                    -<b>Thông Tin Khách Hàng</b><br/>
+                    -<b>Thông Tin Người Dùng</b>          
+                   
+                    </td>
+                </tr>
+            </table>
