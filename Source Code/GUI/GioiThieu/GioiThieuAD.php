3129b36513c2bd8142b3d336afe59f2cdc2053f7
vanthangsqtt <thangbn24896@gmail.com>
2017-07-16 14:21:35 +0700

    Update GioiThieuAD.php

--- Source Code/GUI/GioiThieu/GioiThieuAD.php
+++ Source Code/GUI/GioiThieu/GioiThieuAD.php
@@ -1,6 +1,6 @@
     <table width="640px" id="main-content" border="0px" cellpadding="0px" cellspacing="0px">
             	<tr id="main-navbar" height="36px"class="menu-item" >
-                	<td colspan="6">giới thiệu</td>
+                	<td colspan="6">Giới thiệu</td>
                 </tr>
 				<tr>
                 	<td align="justify" id="info"><b>Chào mừng bạn đến với hệ thống cửa hàng PHƯƠNG NAM .Trang Web gồm những chức năng như sau:<br/>
