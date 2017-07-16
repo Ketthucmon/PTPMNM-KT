c892e4d10da4cb1693292be3285923bb75c3f8a6
khoanguyen1b <khoanguyen8947@gmail.com>
2017-07-16 17:07:30 +0700

    Update XoaSach.php

--- Source Code/XuLy/XuLyXoa/XoaSach.php
+++ Source Code/XuLy/XuLyXoa/XoaSach.php
@@ -2,11 +2,9 @@
 $idtv = $_GET["idtv"];//lấy thông tin mã xóa
 	require("../../Data/Connect.php");
 $sql = "DELETE FROM Sach.Sach WHERE masach= '$idtv'";//thực hiện xóa
-$stmt = db2_prepare($conn, $sql);
-		
+$stmt = db2_prepare($conn, $sql);		
 		if ($stmt) {		
 			$result = db2_execute($stmt);
-
 header("location:admin.php?page=ttsach");//load lại trang
 		}
 ?>
