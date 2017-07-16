99ae09386099d0135999134948db90676aaf9bf2
khoanguyen1b <khoanguyen8947@gmail.com>
2017-07-16 17:07:11 +0700

    Update XoaNXB.php

--- Source Code/XuLy/XuLyXoa/XoaNXB.php
+++ Source Code/XuLy/XuLyXoa/XoaNXB.php
@@ -2,11 +2,9 @@
 $idtv = $_GET["idtv"];//lấy thông tin mã xóa
 	require("../../Data/Connect.php");
 $sql = "DELETE FROM Sach.NXB WHERE manxb= '$idtv'";//thực hiện xóa
-$stmt = db2_prepare($conn, $sql);
-		
+$stmt = db2_prepare($conn, $sql);		
 		if ($stmt) {		
 			$result = db2_execute($stmt);
-
 header("location:admin.php?page=ttnxb");//load lại trang
 		}
 ?>
