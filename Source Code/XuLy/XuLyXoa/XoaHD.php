e72bdb051842a9a45608ad7a9e8c6551511a72ea
khoanguyen1b <khoanguyen8947@gmail.com>
2017-07-16 17:05:39 +0700

    Update XoaHD.php

--- Source Code/XuLy/XuLyXoa/XoaHD.php
+++ Source Code/XuLy/XuLyXoa/XoaHD.php
@@ -8,7 +8,5 @@ $result = db2_execute($stmt);
 $sql1 = "DELETE FROM BanHang.HoaDon WHERE mahd= '$idtv'";//xóa thông tin ở bản chi tiết
 $stmt1 = db2_prepare($conn, $sql1);	
 $result1 = db2_execute($stmt1);
-
-header("location:admin.php?page=tthoadon");//load lại trang
-		
+header("location:admin.php?page=tthoadon");//load lại trang		
 ?>
