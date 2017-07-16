2feb5678d786d37640592d3bfbe75c2dfc28c420
phucbui1b <phucbui1b@gmail.com>
2017-07-16 16:41:57 +0700

    Update ThongTinAD.php

--- Source Code/GUI/FormBanHang/ThongTinAD.php
+++ Source Code/GUI/FormBanHang/ThongTinAD.php
@@ -5,7 +5,6 @@
                 </tr>
 				<tr>
                 	<td align="justify" id="info"><b>Chào mừng bạn đến với hệ thống quản lý cửa hàng sách PHƯƠNG NAM của bán hàng.Trang Web gồm những chức năng như sau:<br/>
-
                     -<b>Thông Tin Hóa Đơn</b><br/>
                     -<b>Thông Tin Khách Hàng</b><br/>
                     -<b>Thông Tin Người Dùng</b>          
