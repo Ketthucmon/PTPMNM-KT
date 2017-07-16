a485b5fb81ee9dc93451464814144d58f927d480
khoanguyen1b <khoanguyen8947@gmail.com>
2017-07-16 17:06:53 +0700

    Update XoaNC.php

--- Source Code/XuLy/XuLyXoa/XoaNC.php
+++ Source Code/XuLy/XuLyXoa/XoaNC.php
@@ -2,11 +2,9 @@
 $idtv = $_GET["idtv"];//lấy thông tin mã xóa
 	require("../../Data/Connect.php");
 $sql = "DELETE FROM QuanLy.DiemDanh WHERE iddd= '$idtv'";//thực hiện xóa
-$stmt = db2_prepare($conn, $sql);
-		
+$stmt = db2_prepare($conn, $sql);		
 		if ($stmt) {		
 			$result = db2_execute($stmt);
-
 header("location:admin.php?page=qlnc");//load lại trang
 		}
 ?>
