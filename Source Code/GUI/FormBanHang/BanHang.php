cf4c005fc095a46bc15665c335d01c9a9597a2d3
phucbui1b <phucbui1b@gmail.com>
2017-07-16 18:04:09 +0700

    Update BanHang.php

--- Source Code/GUI/FormBanHang/BanHang.php
+++ Source Code/GUI/FormBanHang/BanHang.php
@@ -2,7 +2,7 @@
 <?php
 ob_start();
 session_start();
-?>
+?> 
 <!-- Thông tin cấu hình trang page-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
